450c214ddbfb1bd3e70bc6678b1f6a71f19220f9
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-02-05 00:57:12 -0700

    Create MultiThreading.java
    Example usage of Thread Pool Executor Service

---
+++ MultiThreading.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// Example uasge of ThreadPool
+class MultiThreading implements Runnable {
+    int taskId;
+
+    public MultiThreading(int id){
+        taskId = id;
+    }
+
+    @Override
+    public void run(){
+    
+        System.out.println("taskId: "+taskId+"ThreadId: " +Thread.currentThread().getId());
+        
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("task : "+taskId +" completed" + "thread "+ Thread.currentThread().getId());   
+            
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        int n=3;
+        ExecutorService executor = Executors.newFixedThreadPool(n);
+        for(int i=0;i<6;i++){
+            executor.execute(new MultiThreading(i));
+            
+        }
+        
+        executor.shutdown();
+    }
+}
